1a4cce402a3ebd1081581ab586995e9d4dd22aa5
itpteam11 <itpteam11@gmail.com>
2016-05-31 22:47:41 +0800

    Initial commit

---
+++ app/src/main/java/com/itpteam11/visualisemandai/SignInActivity.java
@@ -0,0 +1,87 @@
+package com.itpteam11.visualisemandai;
+
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Activity for user sign in
+ *
+ * This activity allows user to sign in into the application
+ */
+
+public class SignInActivity extends AppCompatActivity implements OnClickListener, OnFocusChangeListener {
+
+    //Declaring activity's widgets
+    private Button btnSignIn;
+    private EditText etEmail, etPassword;
+
+    //For Firebase code testing --------------------------------------------------------------------
+    private Button btnTest;
+    //----------------------------------------------------------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        //Initialising widgets
+        btnSignIn = (Button) findViewById(R.id.signin_button_signin);
+        etEmail = (EditText) findViewById(R.id.signin_edittext_email);
+        etPassword = (EditText) findViewById(R.id.signin_edittext_password);
+
+        //For Firebase code testing ----------------------------------------------------------------
+        btnTest = (Button) findViewById(R.id.signin_button_test);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent firebaseTest = new Intent(SignInActivity.this, FirebaseConnectionActivity.class);
+                startActivity(firebaseTest);
+            }
+        });
+        //------------------------------------------------------------------------------------------
+
+        //Assign on click listener to button
+        btnSignIn.setOnClickListener(this);
+
+        //Assign focus change listener to Edittext to show or hide the soft keyboard
+        etEmail.setOnFocusChangeListener(this);
+        etPassword.setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+
+        //Run respective switch statement accordingly to the pressed button
+        switch(button.getId()) {
+            case R.id.signin_button_signin:
+
+                break;
+        }
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus){
+        //Show soft keyboard when EditText is focused else hide the keyboard. For UX purpose
+        if (hasFocus) {
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+
+            imm.showSoftInput(v, 0);
+        }
+        else {
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        }
+    }
+}
---
+++ app/src/main/java/com/itpteam11/visualisemandai/User.java
@@ -0,0 +1,16 @@
+package com.itpteam11.visualisemandai;
+
+/**
+ *
+ */
+public class User {
+    public String email;
+    public String password;
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
